c38b8d1fa6fb39f26f501101775b345396651d23
Alan Languirand <alan@13protons.com>
2022-02-15 17:11:58 -0500

    fix: all on one page

--- components/WorkList.vue
+++ components/WorkList.vue
@@ -1,67 +1,64 @@
 <template>
-  <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
+    <div class="grid grid-cols-1 xl:grid-cols-2 gap-8">
       <nuxt-link
         v-for="item in features"
         :key="item.title"
         class="
-          bg-gradient-to-br
+          
+           bg-gradient-to-br
 
-          from-stone-200
+          from-white
           to-stone-100
 
           dark:from-stone-900
           dark:to-stone-800
-          
-          rounded-lg
-          cursor-pointer
-          flex flex-col
-          
+                    
           border-2
           border-slate-300
           dark:border-slate-900
           hover:border-cyan-500
           dark:hover:border-amber-500
+          
+          rounded-lg
+          cursor-pointer
+          flex flex-col
+          md:flex-row md:h-64
 
           transition-border-color
           duration-100
           transition-translate
           hover:-translate-y-1
           active:translate-y-1
-          h-72
+          flex flex-col
         "
         :to="`/work/${item.slug}`"
       >
-        <!-- <div
+        <div
           class="
             bg-slate-800
-            h-32
-            shrink-0
+            h-48
+            min-w-[16em]
             w-full
             rounded-t-lg
+            md:rounded-l-lg md:rounded-tr-none md:w-64 md:h-full
             bg-cover
-          "
-          :class="item.heroClass"
-          :style="{ backgroundImage: `url(/work/${featuredImage(item)}?resize&size=300&format=jpg)` }"
-        ></div> -->
 
-         <div
-          class="
-            bg-slate-800
-            h-32
-            shrink-0
-            w-full
-            rounded-t-lg
-            bg-cover
+            md:border-r-2
+            md:border-b-0
+            border-b-2
+            
+            border-slate-300
+            dark:border-slate-900
           "
           :class="item.heroClass"
-          :style="{ backgroundImage: `url(${require(`@/assets/img/work/${featuredImage(item)}?resize&size=300&format=jpg`)})` }"
+          :style="{ backgroundImage: `url(${require(`@/assets/img/work/${featuredImage(item)}?resize&size=520&format=jpg`)})` }"
         ></div>
-        <div class="p-5 grow flex flex-col">
+        <div class="p-5 flex flex-col">
           
             <div class="mb-4">
-              <h3 class="text-lg font-bold">
+              <h4 class="text-lg font-bold">
                 {{ item.title }}
-              </h3>
+              </h4>
             </div>
             <div class="grow">
               <p class="text-ellipsis">{{ item.description }}</p>
@@ -73,48 +70,42 @@
             </div>
         </div>
       </nuxt-link>
-  </div>  
+    </div>
 </template>
 
 <script>
-import Debug from 'debug';
-const debug = Debug('13protons.components.worklist');
+import Debug from 'debug'
+const debug = Debug('13protons.components.worklist')
 export default {
-  name: 'MainMenu',
-  props: {
-    featured: { type: Boolean, default: false }
-  },
+  name: 'WorkList',
   data() {
     return {
-      items: {}
+      items: {},
     }
   },
   async fetch() {
-
-    this.items = await this.$nuxt.context.$content('/work')
-                        .where({ featured: true })
-                        .sortBy('order', 'desc')
-                        .limit(4)
-                        .fetch()
-                        .catch(err => {
-                          debug('err', err);
-                        });
-    
+    this.items = await this.$nuxt.context
+      .$content('/work')
+      .sortBy('order', 'desc')
+      .fetch()
+      .catch((err) => {
+        debug('err', err)
+      })
   },
- 
+
   computed: {
     features() {
-      return this.items || [];
-    }
+      return this.items || []
+    },
   },
   methods: {
-   featuredImage(item) {
+    featuredImage(item) {
       if (item.images) {
         return item.images[0].path
       }
       return ''
     }
-  }  
+  }
 }
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -9,32 +9,7 @@
     <div>
       <h2 class="text-4xl font-bold mb-8">Here's some things I've done</h2>
       <WorkList />
-      <p class="text-center p-8 mt-4">
-        <nuxt-link
-          to="/work"
-          class="
-            rounded-md
-            bg-cyan-700
-            dark:bg-yellow-500
-            text-white
-            dark:text-slate-900
-            px-6
-            py-2
-            border-2 
-            border-slate-900
-            
-            hover:border-cyan-500
-            dark:hover:border-amber-700
-            transition-border-color
-            duration-100
-            transition-translate
-            hover:-translate-y-1
-            active:translate-y-1
-          "
-        >
-          See More...
-        </nuxt-link>
-      </p>
+
     </div>
 
     <!-- <div>03</div> -->
