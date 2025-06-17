bc13666a782a6351aa93ed45dbedf8ed9fb8aba4
Alan Languirand <alan@13protons.com>
2025-06-16 22:18:59 -0400

    Update Contact.vue

--- components/Contact.vue
+++ components/Contact.vue
@@ -2,7 +2,7 @@
   <span class="">
     <span class="link cursor-pointer" @click="trymail">hello [at] 13protons.com</span> -
     <StyleLink href="https://www.linkedin.com/in/13protons/">LinkedIn</StyleLink> -
-    <StyleLink href="https://github.com/13protons">Github</StyleLink>
+    <StyleLink href="https://github.com/13protons">GitHub</StyleLink>
     <!-- <StyleLink href="https://twitter.com/13protons">Twitter</StyleLink> -->
      <br/>
     👉 Detroit, MI
